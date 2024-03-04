5b8ed8127b81ad0b57a170eea5e471404ad85d41
AndresS <andressanchezzu@gmail.com>
2024-03-04 14:03:31 -0600

    Realizando cambios en la interfaz principal

--- views/index.php
+++ views/index.php
@@ -7,7 +7,7 @@
     <title>JAFRA</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-        <link rel="stylesheet" href="./assets/index.css">
+    <link rel="stylesheet" href="./assets/index.css">
 </head>
 
 <body>
@@ -54,58 +54,55 @@
     </nav>
 
     <main>
-        <div class="container-cards">
-            <div class="row row-cols-1 row-cols-md-2 g-4">
+
+        <div class="container-frase">
+            <h4> Especialidad en motores, cajas de cambios y partes eléctricas. <br>
+                Pruebas de distribuidores, igniter, bobinas, etc.</h4>
+        </div>
+
+
+        <div id="carouselExample" class="carousel slide">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="https://www.acmonedero.com/wp-content/themes/yootheme/cache/45/img-caja-cambios-mecanica@2x-454e3d16.jpeg"
+                        class="d-block w-100" alt="..." id="imagenCarrusel">
+                </div>
+                <div class="carousel-item">
+                    <img src="https://motor.elpais.com/wp-content/uploads/2022/09/alternador.jpg?v=1663843719"
+                        class="d-block w-100" alt="..." id="imagenCarrusel">
+                </div>
+                <div class="carousel-item">
+                    <img src="https://img.freepik.com/fotos-premium/motor-aislado-sobre-fondo-blanco-ai-generativo_971989-7417.jpg"
+                        class="d-block w-100" alt="..." id="imagenCarrusel">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </main>
+
+    <footer>
+        <div class="container text-center">
+            <div class="row">
                 <div class="col">
-                    <div class="card">
-                        <img src="https://www.acmonedero.com/wp-content/themes/yootheme/cache/45/img-caja-cambios-mecanica@2x-454e3d16.jpeg" class="card-img-top" id="imagenCard" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Caja de cambios</h5>
-                            <p class="card-text">Componente crucial en un vehículo que permite variar la relación de velocidad entre el motor y las ruedas. </p>
-                        </div>
-                    </div>
+                    <h4 class="infoC">Contacto: 2286-1263</h4>
                 </div>
                 <div class="col">
-                    <div class="card">
-                        <img src="https://motor.elpais.com/wp-content/uploads/2022/09/alternador.jpg?v=1663843719" class="card-img-top"  id="imagenCard" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Alternador</h5>
-                            <p class="card-text">Un alternador es un componente esencial en un vehículo que tiene la función de generar energía eléctrica para alimentar 
-                                el sistema eléctrico del automóvil y recargar la batería.</p>
-                        </div>
-                    </div>
+                <h4 class="infoC"><a href="https://www.facebook.com/repuestosjafra" id="facebook" target="_blank">Facebook</a></h4>
                 </div>
                 <div class="col">
-                    <div class="card">
-                        <img src="..." class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-                                to
-                                additional content.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card">
-                        <img src="..." class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-                                to
-                                additional content. This content is a little bit longer.</p>
-                        </div>
-                    </div>
+                <h4 class="infoC">2024 FAJRA</h4>
                 </div>
             </div>
         </div>
 
-
-        <div class="container-frase">
-            <h4> Especialidad en motores, cajas de cambios y partes eléctricas. <br>
-            Pruebas de distribuidores, igniter, bobinas, etc</h4>
-        </div>
-    </main>
+    </footer>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"

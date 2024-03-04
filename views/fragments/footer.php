6b5b98e2f6119fd12523463c576d472d3efde90c
AndresS <andressanchezzu@gmail.com>
2024-03-04 14:33:03 -0600

    creando la carpeta fragments para agregar el footer

---
+++ views/fragments/footer.php
@@ -0,0 +1,16 @@
+<footer>
+        <div class="container text-center">
+            <div class="row">
+                <div class="col">
+                    <h4 class="infoC">Contacto: 2286-1263</h4>
+                </div>
+                <div class="col">
+                <h4 class="infoC"><a href="https://www.facebook.com/repuestosjafra" id="facebook" target="_blank">Facebook</a></h4>
+                </div>
+                <div class="col">
+                <h4 class="infoC">2024 FAJRA</h4>
+                </div>
+            </div>
+        </div>
+
+    </footer>
--- views/index.php
+++ views/index.php
@@ -7,7 +7,8 @@
     <title>JAFRA</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="./assets/index.css">
+    <link rel="stylesheet" href="./assets/css/index.css">
+    <link rel="stylesheet" href="./assets/css/footer.css">
 </head>
 
 <body>
@@ -87,22 +88,9 @@
         </div>
     </main>
 
-    <footer>
-        <div class="container text-center">
-            <div class="row">
-                <div class="col">
-                    <h4 class="infoC">Contacto: 2286-1263</h4>
-                </div>
-                <div class="col">
-                <h4 class="infoC"><a href="https://www.facebook.com/repuestosjafra" id="facebook" target="_blank">Facebook</a></h4>
-                </div>
-                <div class="col">
-                <h4 class="infoC">2024 FAJRA</h4>
-                </div>
-            </div>
-        </div>
-
-    </footer>
+    <?php
+    include "./fragments/footer.php"
+    ?>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"

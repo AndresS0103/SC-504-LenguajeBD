c528889e81b6710ae7db4d949ac88f6e2bee47aa
FabricioMontanari <fabriciomontanariv@gmail.com>
2024-03-22 22:01:03 -0600

    conexion a la bd

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const oracledb = require('oracledb');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'views')));
+
+app.get('/usuarios', async (req, res) => { 
+    let connection;
+    try {
+        connection = await oracledb.getConnection({
+            user: "hr",
+            password: "12345",
+            connectString: "localhost/orcl"
+        });
+
+        const result = await connection.execute(`SELECT * FROM usuario`);
+        const usuarios = result.rows; 
+        res.json(usuarios); 
+    } catch (error) {
+        console.error('Error al obtener los usuarios:', error);
+        res.status(500).json({ error: 'Error al obtener los usuarios' });
+    } finally {
+        if (connection) {
+            try {
+                await connection.close();
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    }
+});
+
+
+app.listen(3000, () => {
+    console.log('Servidor en el puerto 3000');
+});

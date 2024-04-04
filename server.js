e06164248e6cc00707b5e35b2d6b00879d697af0
Andres Sanchez <xxtherealdresxx@gmail.com>
2024-04-04 16:59:40 -0600

    implementando el agregar usuarios

--- server.js
+++ server.js
@@ -15,13 +15,13 @@ const dbConfig = {
     user: 'hr',
     password: 'Hola123456789',
     connectString: 'localhost/orcl'
-  };
+};
 
 //ver usuarios
-app.get('/usuarios', async (req, res) => { 
+app.get('/usuarios', async (req, res) => {
     try {
         const usuarios = await obtenerUsuarios();
-        res.json(usuarios); 
+        res.json(usuarios);
     } catch (error) {
         console.error('Error al obtener los usuarios:', error);
         res.status(500).json({ error: 'Error al obtener los usuarios' });
@@ -30,38 +30,58 @@ app.get('/usuarios', async (req, res) => {
 
 //insertar usuarios
 app.post('/usuariosInsertar', async (req, res) => {
+    const { id_usuario, nombre, prim_apellido, seg_apellido, cedula, rol, telefono_usuario, correo } = req.body;
+
     try {
-        const { id_usuario, nombre, prim_apellido, seg_apellido, cedula, rol, telefono_usuario, correo } = req.body;
-        await insertarUsuario(id_usuario, nombre, prim_apellido, seg_apellido, cedula, rol, telefono_usuario, correo);
-        res.status(201).json({ message: 'Usuario insertado correctamente' });
+        const connection = await oracledb.getConnection(dbConfig);
+        const result = await connection.execute(
+            `BEGIN paquete_usuarios.INSERTAR_USUARIO(:id_usuario, :nombre, :prim_apellido, :seg_apellido, :cedula, :rol, :telefono_usuario, :correo); END;`,
+            {
+                id_usuario: id_usuario,
+                nombre: nombre,
+                prim_apellido: prim_apellido,
+                seg_apellido: seg_apellido,
+                cedula: cedula,
+                rol: rol,
+                telefono_usuario: telefono_usuario,
+                correo: correo
+            },
+            { autoCommit: true }
+        );
+
+        connection.close();
+        res.status(200).json({ message: 'Usuario agregado exitosamente' });
     } catch (error) {
-        console.error('Error al insertar el usuario:', error);
-        res.status(500).json({ error: 'Error interno del servidor al insertar el usuario' });
+        console.error('Error al insertar usuario:', error);
+        res.status(500).json({ error: 'Error interno al insertar usuario' });
     }
 });
+
+
+
 
 
 
 //crear facturas
 app.post('/facturas', async (req, res) => {
     const { id_factura, id_usuario, fecha_pago, total_pago } = req.body;
-  
+
     try {
-      const connection = await oracledb.getConnection(dbConfig);
-      const result = await connection.execute(
-        `INSERT INTO factura (id_factura, id_usuario, fecha_pago, total_pago) VALUES (:id_factura, :id_usuario, TO_DATE(:fecha_pago, 'YYYY-MM-DD'), :total_pago)`,
-        [id_factura, id_usuario, fecha_pago, total_pago],
-        { autoCommit: true }
-      );
-  
-      connection.close();
-      res.status(200).json({ message: 'Factura agregada exitosamente' });
+        const connection = await oracledb.getConnection(dbConfig);
+        const result = await connection.execute(
+            `INSERT INTO factura (id_factura, id_usuario, fecha_pago, total_pago) VALUES (:id_factura, :id_usuario, TO_DATE(:fecha_pago, 'YYYY-MM-DD'), :total_pago)`,
+            [id_factura, id_usuario, fecha_pago, total_pago],
+            { autoCommit: true }
+        );
+
+        connection.close();
+        res.status(200).json({ message: 'Factura agregada exitosamente' });
     } catch (error) {
-      console.error('Error al insertar factura:', error);
-      res.status(500).json({ error: 'Error interno al insertar factura' });
+        console.error('Error al insertar factura:', error);
+        res.status(500).json({ error: 'Error interno al insertar factura' });
     }
-  });
-  
+});
+
 
 // ver facturas
 app.get('/verFacturas', async (req, res) => {
